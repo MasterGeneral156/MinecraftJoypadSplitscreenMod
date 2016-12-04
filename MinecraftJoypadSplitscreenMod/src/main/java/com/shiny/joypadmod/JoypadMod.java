2f6867a4da5df9ca4cfd7f37125400554d281768
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2016-12-03 21:33:14 -0500

    1.11. Should work.

--- MinecraftJoypadSplitscreenMod/src/main/java/com/shiny/joypadmod/JoypadMod.java
+++ MinecraftJoypadSplitscreenMod/src/main/java/com/shiny/joypadmod/JoypadMod.java
@@ -20,7 +20,7 @@ import com.shiny.joypadmod.minecraftExtensions.JoypadMouseHelper;
 
 
 @Mod(modid = JoypadMod.MODID, name = JoypadMod.NAME, version = ModVersionHelper.VERSION + "-" + JoypadMod.MINVERSION
-		+ JoypadMod.REVISION, clientSideOnly = true, acceptedMinecraftVersions = "[1.10.0,)")
+		+ JoypadMod.REVISION, clientSideOnly = true, acceptedMinecraftVersions = "[1.11,)")
 // 1.6.4
 // @NetworkMod(serverSideRequired = false)
 public class JoypadMod
